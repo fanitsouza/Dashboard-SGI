ef575311a0a46eb6ed4c77e620aa73a0f18a5f5f
Fani Tamires <2023002117@ifam.edu.br>
2024-08-12 16:31:10 -0400

    Atualização do dashboard

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function toggleMenu(){
-    let toggle = document.querySelector('.toggle');
+let toggle = document.querySelector('.toggle');
     let navigation = document.querySelector('.navigation');
     let main = document.querySelector('.main');
+
+function toggleMenu(){
     toggle.classList.toggle('active');
     navigation.classList.toggle('active');
     main.classList.toggle('active');
 }
+
+let list = document.querySelectorAll('.navigation li');
+function activeLink(){
+    list.forEach((item) => 
+    item.classList.remove('hovered'));
+    this.classList.add('hovered');
+}
+
+
